78f155ea3c3b500e274bd351fdd8ada50d98ab35
Joehanz <yohanes2000@gmail.com>
2026-06-05 09:47:46 +0700

    Update manual-watch.js

--- manual-watch.js
+++ manual-watch.js
@@ -43,7 +43,7 @@ function loadMovie() {
     </p>
 
     <!-- TOMBOL VERSI ORISINIL MENGARAH KE WATCH.HTML SESUAI GRID INDEX -->
-    <div style="display;none; margin:20px 0 15px 0; padding:14px; border-radius:12px; background:#111; border:1px solid rgba(255,255,255,.08);">
+    <div style="display:none; margin:20px 0 15px 0; padding:14px; border-radius:12px; background:#111; border:1px solid rgba(255,255,255,.08);">
       <a href="watch.html?movie=${movieId}" style="color:gold; font-weight:bold; text-decoration:none; display:inline-block;">
         🎬 Versi Orisinal
       </a>
