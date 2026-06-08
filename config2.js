80aa66dd7f1a150b96d3d65c1f0007c1fe0677f7
Joehanz <yohanes2000@gmail.com>
2026-06-08 10:18:05 +0700

    Create config2.js

---
+++ config2.js
@@ -0,0 +1,21 @@
+const CONFIG2={
+
+TMDB_KEY:"PASTE_TMDB_API_KEY_HERE",
+
+TMDB_BASE:
+"https://api.themoviedb.org/3",
+
+TMDB_IMAGE:
+"https://image.tmdb.org/t/p/original",
+
+MOVIES_JSON:
+"movies.json",
+
+PLAYER_HEIGHT:
+"77vh",
+
+RELATED_LIMIT:8,
+
+RANDOMIZE:true
+
+};
