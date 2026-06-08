a7ac29a951014c4a90af16b191b3e19406852948
Joehanz <yohanes2000@gmail.com>
2026-06-08 10:55:19 +0700

    Update config2.js

--- config2.js
+++ config2.js
@@ -1,21 +1,44 @@
 const CONFIG2={
 
-TMDB_KEY:"b3b893873ed1bb7f175b2707afeea2a0",
+/* TMDB */
+
+TMDB_KEY:
+"PASTE_TMDB_API_KEY_HERE",
 
 TMDB_BASE:
 "https://api.themoviedb.org/3",
 
 TMDB_IMAGE:
+"https://image.tmdb.org/t/p/w500",
+
+TMDB_BACKDROP:
 "https://image.tmdb.org/t/p/original",
+
+
+/* DATA */
 
 MOVIES_JSON:
 "movies.json",
 
+
+/* GRID */
+
+POSTER_PER_PAGE:
+26,
+
+RANDOMIZE:
+true,
+
+
+/* RELATED */
+
+RELATED_LIMIT:
+15,
+
+
+/* PLAYER */
+
 PLAYER_HEIGHT:
-"77vh",
-
-RELATED_LIMIT:8,
-
-RANDOMIZE:true
+"77vh"
 
 };
