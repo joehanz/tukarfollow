60446140abcfd97ad790f4f0d4129197a0f002f6
Joehanz <yohanes2000@gmail.com>
2026-07-05 21:16:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-const API_KEY = "c000d7b8b0f5ee16b98b6103009745d8";
+const API_KEY = "c000d7b8b0f5ee16b98b6103009745d8"; // v3 key valid
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const grid = document.getElementById("movie-grid");
 
 async function getMovies() {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-  const data = await res.json();
-  console.log("TMDB response:", data);
+  try {
+    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+    const data = await res.json();
+    console.log("TMDB response:", data);
 
-  if (!data.results) {
-    grid.innerHTML = "<p style='color:white'>API gagal, cek console.</p>";
-    return;
-  }
-
-  data.results.forEach(movie => {
-    if (movie.poster_path) {
-      const img = document.createElement("img");
-      img.src = IMG_URL + movie.poster_path;
-      img.alt = movie.title;
-      img.onclick = () => {
-        window.location.href = `watch.html?id=${movie.id}`;
-      };
-      grid.appendChild(img);
+    if (!data.results) {
+      grid.innerHTML = "<p style='color:white'>API gagal, cek console.</p>";
+      return;
     }
-  });
+
+    data.results.forEach(movie => {
+      if (movie.poster_path) {
+        const img = document.createElement("img");
+        img.src = IMG_URL + movie.poster_path;
+        img.alt = movie.title;
+        img.onclick = () => {
+          window.location.href = `watch.html?id=${movie.id}`;
+        };
+        grid.appendChild(img);
+      }
+    });
+  } catch (err) {
+    console.error("Error fetching movies:", err);
+    grid.innerHTML = "<p style='color:white'>Fetch error, cek console.</p>";
+  }
 }
 getMovies();
