b1a600f117b579314d78b838bb3ea1f5f7254070
Joehanz <yohanes2000@gmail.com>
2026-07-05 20:57:30 +0700

    Change TMDB API key in script.js
    Updated the API key for TMDB.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_TMDB_API_KEY";
+const API_KEY = "0ca481842a3f253ed72008c1d9e9f2e6";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const grid = document.getElementById("movie-grid");
 
