1129d44f15ee8ea9a80abeb7ce2e60e3b665673e
Joehanz <yohanes2000@gmail.com>
2026-07-05 21:11:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500";
 const grid = document.getElementById("movie-grid");
 
 async function getMovies() {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+  const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
   const data = await res.json();
+  console.log("TMDB response:", data);
+
+  if (!data.results) {
+    grid.innerHTML = "<p style='color:white'>API gagal, cek console.</p>";
+    return;
+  }
+
   data.results.forEach(movie => {
-    const img = document.createElement("img");
-    img.src = IMG_URL + movie.poster_path;
-    img.onclick = () => {
-      window.location.href = `watch.html?id=${movie.id}`;
-    };
-    grid.appendChild(img);
+    if (movie.poster_path) {
+      const img = document.createElement("img");
+      img.src = IMG_URL + movie.poster_path;
+      img.alt = movie.title;
+      img.onclick = () => {
+        window.location.href = `watch.html?id=${movie.id}`;
+      };
+      grid.appendChild(img);
+    }
   });
 }
 getMovies();
